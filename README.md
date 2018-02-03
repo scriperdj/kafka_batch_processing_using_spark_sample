b77269a56074e7da198abd68c0fe237dad144e94
scriperdjq <sathish@quartic.ai>
2018-02-03 19:30:56 +0530

    added readme & some other changes

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Batch processing of multi partitioned Kafka topics using Spark 
+# Example Spark Application for Batch processing of multi partitioned Kafka topics
+
+This example application reads given Kafka topic & broker details and does below operations
+* Get partition & offset details of provided Kafka topics.
+* Create DataFrame with the data read.
+* **Find Top trending product in each category based on users browsing data.**
+
+The output is not implemented. It just displays the Top 10 Product/Category in logs.
+
+Please read the blog post [in here](http://sathish.me/scala/2018/02/03/batch-processing-of-multi-partitioned-kafka-topics-using-spark-with-example.html) to know about the approach & introduction about the problem.
+
+## Build
+```bash
+> sbt package
+```
+
+## Execution
+
+* Create kafka topic with required number of partition & other configurations
+```bash
+> kafka-topics --zookeeper localhost:2181 --replication-factor 1 --create --partitions 5 --topic web_stream --config retention.ms=604800000
+```
+* Generate Sample data for the topic
+```bash
+> kafka-console-producer --broker-list localhost:9092 --topic web_stream  --property parse.key=true --property key.separator=,
+> cus_001,{"product":"PD0021","category": "Books","ts":"1516978415"}
+> cus_001,{"product":"PD0022","category": "TV","ts":"1517978415"}
+```
+* Submit Spark job
+```bash
+> spark-submit --packages org.apache.kafka:kafka-clients:0.11.0.0,org.apache.spark:spark-streaming-kafka-0-10_2.11:2.2.0 --class example.spark.BatchProcessKafka --master yarn target/scala-2.11/kafka_batch_processing_using_spark_sample_2.11-1.0.jar web_stream localhost:9092
+```
--- src/main/scala/example/spark/BatchProcessKafka.scala
+++ src/main/scala/example/spark/BatchProcessKafka.scala
@@ -1,7 +1,11 @@
 package example.spark
-
+/**
+ * This Spark application finds top Products in each Category from Users browsing data
+ *
+ */
 import org.apache.spark.{ SparkConf, SparkContext, _}
 import org.apache.spark.sql.{DataFrame, SQLContext, SparkSession}
+import org.apache.spark.sql.expressions._
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
 import org.apache.spark.streaming.kafka010._
@@ -10,18 +14,15 @@ import org.apache.kafka.clients.admin.{AdminClient,NewTopic,AdminClientConfig}
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import org.apache.kafka.common.TopicPartition
 
-
-import org.yaml.snakeyaml.Yaml
-import org.yaml.snakeyaml.constructor.Constructor
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.{File, FileInputStream}
 import java.util.Properties
 import java.time.Instant
 import collection.JavaConverters._
 import collection.JavaConversions._
+import scala.util.parsing.json._
+import scala.util.Try
 
 object BatchProcessKafka{
 
@@ -29,9 +30,8 @@ object BatchProcessKafka{
   val LOG = LoggerFactory.getLogger(BatchProcessKafka.getClass);
 
   def main(args: Array[String]): Unit = {
-    val file = new FileInputStream(new File(args(0)))
-    val yaml = new Yaml(new Constructor(classOf[Config]))
-    val config = yaml.load(file).asInstanceOf[Config]
+    val inputTopic = args(0)
+    val kafkaBrokers = args(1)
 
     val conf = new SparkConf().setAppName(APP_NAME)
     val sc = new SparkContext(conf)
@@ -42,7 +42,7 @@ object BatchProcessKafka{
     val gid = APP_NAME + Instant.now.getEpochSecond
 
     val kafkaParams = scala.collection.immutable.Map[String, Object](
-      "bootstrap.servers" -> config.kafkaBrokers,
+      "bootstrap.servers" -> kafkaBrokers,
       "key.deserializer" -> classOf[StringDeserializer],
       "value.deserializer" -> classOf[StringDeserializer],
       "group.id" -> gid,
@@ -51,29 +51,61 @@ object BatchProcessKafka{
     ).asJava
 
     val KafkaConfig = new Properties()
-    KafkaConfig.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, config.kafkaBrokers)
+    KafkaConfig.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBrokers)
     val adminClient = AdminClient.create(KafkaConfig)
 
     val consumer = new KafkaConsumer[String,String](kafkaParams)
 
+    // Get list of all partitions of given Topic
     val topicPartitions = adminClient
-                          .describeTopics(List[String](config.inputTopic).asJava)
-                          .all().get().get(config.inputTopic).partitions()
+                          .describeTopics(List[String](inputTopic).asJava)
+                          .all().get().get(inputTopic).partitions()
 
+    // Create Array of OffsetRange with topic, partition number, start & end offsets
     val offsetRanges = topicPartitions.asScala.map(x =>{
-      val topicPartition = new TopicPartition(config.inputTopic, x.partition)
+      val topicPartition = new TopicPartition(inputTopic, x.partition)
       val startOffset = consumer.beginningOffsets(List[TopicPartition](topicPartition))
                         .values().asScala.toList.get(0)
       val stopOffset = consumer.endOffsets(List[TopicPartition](topicPartition))
                         .values().asScala.toList.get(0)
       OffsetRange(topicPartition,
-                  startOffset,
-                  stopOffset)
+                    startOffset,
+                    stopOffset)
     }).toArray
 
+    // Create RDD from provided topic & offset details
     val messagesRDD = KafkaUtils.createRDD[String, String](sc, kafkaParams,
                           offsetRanges, PreferConsistent)
 
+    // Convert to DataFrame with columns "customer_id","product","category","ts"
+    val eventsDF = messagesRDD.map(message =>{
+      val value = message.value().asInstanceOf[String]
+      val key = message.key().asInstanceOf[String]
+      val msgValues = JSON.parseFull(value).get.asInstanceOf[Map[String, String]]
+      (key,msgValues("product"),msgValues("category"),msgValues("ts").toLong)
+    }).toDF("customer_id","product","category","ts")
 
+    // Compute unique Product/Category count
+    val productsDF = eventsDF.groupBy("product","category").count()
+
+    productsDF.createOrReplaceTempView("product_category_count")
+
+    // Find Top Products in each Category
+    val topProducts = sqlContext.sql("""SELECT x.category,
+                                               x.product,
+                                               x.count
+                                        from product_category_count x
+                                        JOIN (
+                                          SELECT p.category,
+                                                 max(count) AS max_count
+                                          FROM product_category_count p
+                                          GROUP BY p.category) y
+                                        ON y.category = x.category
+                                        AND y.max_count = x.count""")
+    topProducts.show()
+
+    // Write to DB or send to downstream Kafka (To be implemented)
+
+    sc.stop()
   }
 }
